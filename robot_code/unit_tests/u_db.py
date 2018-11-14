a8b7206873d240b52ed832a4a98760b1b3594424
penguinarmy94 <luisote94@gmail.com>
2018-11-14 13:25:27 -0800

    created database read() test

---
+++ robot_code/unit_tests/u_db.py
@@ -0,0 +1,46 @@
+import unittest
+import time
+import json
+import sys, os
+from sys import path
+path.insert(0, "C:/Users/Luis/Desktop/Fall 2018/CMPE 295A/Robot Code/robot_code")
+from build import queues, database, behavior
+#import app
+
+
+class DatabaseTest(unittest.TestCase):
+    __db = None
+    __robot = None
+
+    def setUp(self):
+        self.__robot = behavior.Behavior()
+        self.__robot.battery = 50
+
+        config_file = open('../config.json')
+        config = json.load(config_file)
+        config_file.close()
+
+        self.__robot.robotid = config["robotid"]
+        self.__robot.connection = 5
+        self.__db = database.Database(queues.brain_database_queue, self.__robot)
+    
+    def tearDown(self):
+        print("Database stopped")
+        #self.__db.close()
+        #queues.brain_motor_queue.put(json.dumps({"type": "destruct", "message": "Turn off system"}))
+
+    def test_read(self):
+        self.__db.read()
+
+        self.assertEqual(self.__robot.robotid, "01A1Z100BY")
+        self.assertEqual(self.__robot.userid.get().to_dict()["username"], "rosellc@gmail.com")
+        self.assertEqual(self.__robot.name, "robot1")
+    
+    """
+    def test_queue(self):
+        queues.arduino_motor_queue.put("5")
+        print(queues.arduino_motor_queue)
+    """
+                
+if __name__ == "__main__":
+    unittest.main()
