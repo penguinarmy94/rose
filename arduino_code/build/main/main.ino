220b2e3d83c8f5263b2974ed4c53f809c36cba44
Andrew Javier <diarkia124@yahoo.com>
2019-01-01 14:45:06 -0800

    Added message parsing from Pi

--- arduino_code/build/main/main.ino
+++ arduino_code/build/main/main.ino
@@ -1,6 +1,11 @@
 #include "Motor.h"
 #include "Microphone.h"
 
+struct PIData {
+  char direction;
+  int distance;
+  };
+  
 Motor left(12, 13, 11);
 Motor right(7, 8, 9);
 void setup() 
@@ -41,4 +46,23 @@ void Halt(Motor left, Motor right)
 {
   left.halt();
   right.halt();
+}
+
+void parsePackage(PIData &package)
+{
+  Serial.readBytesUntil('-', fromPi, 10); 
+  package.direction = fromPi[0];
+  int dist = 0;
+  int i = 1;
+  while(fromPi[i] >= '0' && i < 10)
+    {
+      dist *= 10;
+      dist += (fromPi[i] - '0');
+      i++;
+    }
+    package.distance = dist;
+  for (int j = 0; j < 10; j++)
+    {
+      fromPi[j] = 0;  
+    }
 }
