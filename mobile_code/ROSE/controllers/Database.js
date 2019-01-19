361f0d7e7727ec6838899f03f6cbae2259035377
penguinarmy94 <luisote94@gmail.com>
2019-01-19 01:50:09 -0800

    Updated Database logic for handling Google Cloud Firestore calls

--- mobile_code/ROSE/controllers/Database.js
+++ mobile_code/ROSE/controllers/Database.js
@@ -3,21 +3,53 @@ import firebase from 'react-native-firebase';
 export default class Database {
     constructor() {
         this._userRef = firebase.firestore().collection('Users');
-        this._behaviorRef = firebase.firestore().collection('Behaviors');
-        this._robotRef = firebase.firestore().collection('Robots');
         this._actionRef = firebase.firestore().collection('Action');
+        this._defaultBehavior = firebase.firestore().collection('Behaviors').doc('default');
     }
 
-    getBehaviors = (username) => {
+    getUser = (id) => {
+        let result = this._userRef.doc(id);
+
+        if(result) {
+            return result;
+        }
+        else {
+            return "no user";
+        }
+    }
+
+    getBehavior = (id) => {
 
     }
 
-    getRobots = (username) => {
+    getRobot = (id) => {
 
     }
 
     getActions = () => {
+        this._actionRef.get().then((snapshot) => {
+            let actionList = [];
+            snapshot.forEach((doc) => {
+                actionList.push(doc.data());
+            });
+            return actionList;
+        }).catch((error) => {
+            alert("No actions available");
+        });
+    }
 
+    updateUser = (user, id = 0, newRobot = null, type) => {
+        if (type == "add") {
+            let user = {
+                username: user,
+                behaviors: [this._defaultBehavior],
+                robots: [newRobot]
+            };
+            this._userRef.doc(id).set(user);
+        }
+        else if (type == "update") {
+            this._userRef.doc(id).set(user);
+        }
     }
 
     updateBehavior = (behavior, type) => {
