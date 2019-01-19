3ad79b8cdef8a3b1d4ef59bc26e49da85c74dfe9
penguinarmy94 <luisote94@gmail.com>
2019-01-19 01:55:08 -0800

    removed unused variables and imports

--- mobile_code/ROSE/App.js
+++ mobile_code/ROSE/App.js
@@ -7,16 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, Image, Button} from 'react-native';
 import AppStack from './navigators/AppStack';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 
 export default class App extends Component {
   render() {
