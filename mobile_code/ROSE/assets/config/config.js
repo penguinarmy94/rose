2418abb52bb41805d23a9cc89b657ae5894bf9a4
penguinarmy94 <luisote94@gmail.com>
2019-01-22 13:53:53 -0800

    Added new config folder to add all constants

---
+++ mobile_code/ROSE/assets/config/config.js
@@ -0,0 +1,7 @@
+
+let config = {
+    errorCodes: ["auth/invalid-email", "auth/user-disabled", "auth/user-not-found", "auth/wrong-password"],
+    debugId: "E2OEZaN3UQUZUncvnprtbUjKQ0W2",
+}
+
+export {config};
