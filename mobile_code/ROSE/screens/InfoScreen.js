096f9f839e004c57d7057eb216821b361d846905
penguinarmy94 <luisote94@gmail.com>
2019-01-22 15:18:26 -0800

    Added promise handling, real-time info update

--- mobile_code/ROSE/screens/InfoScreen.js
+++ mobile_code/ROSE/screens/InfoScreen.js
@@ -3,31 +3,86 @@ import {Platform, StyleSheet, Text, View, TextInput, Image, Button} from 'react-
 import firebase from 'react-native-firebase';
 import NotificationManager from '../controllers/NotificationManager';
 import Session from '../controllers/Session';
+import User from '../controllers/User';
+
+let isMounted = false;
 
 export default class InfoScreen extends Component {
   constructor(props) {
     super(props);
 
-    this.updateInfo = this.updateInfo.bind(this);
-    //this.changePowerOption = this.changePowerOption.bind(this);
+    this.state = {
+      id: 0,
+      battery: 0,
+      connection: 0,
+      charging: false,
+      idle: "none",
+      detect: "none",
+      recordings: 0,
+      connectionColor: "green",
+      batteryColor: "green"
+    };
 
-    let id = this.props.screenProps.data;
-    this.session = new Session(id);
+    this.session = this.props.screenProps.data;
+    isMounted = true;
+
+    alert("Welcome back!");
+
+    this.session.createUser().then((user) => {
+      if(user.exists) {
+        this.session.setUser(new User(user.data()));
+        this.robotSnapshot = this.session.currentRobot().onSnapshot((robot) => {
+          if(robot.exists && isMounted) {
+            let idle = robot.data().idle_behavior;
+            let detect = robot.data().detect_behavior;
+            this.currentRobot = robot.data();
+            this.currentRobot.idle_behavior = "";
+            this.currentRobot.detect_behavior = "";
+
+            idle.get().then((doc) => { 
+              if(doc.exists && isMounted) { 
+                this.currentRobot.idle_behavior = doc.data().name;
+                this.updateInfo();
+              }
+            }).catch((error) => {
+              alert(error);
+            });
+
+            detect.get().then((doc) => {
+              if(doc.exists && isMounted) {
+                this.currentRobot.detect_behavior = doc.data().name;
+                this.updateInfo();
+              }
+            }).catch((error) => {
+              alert(error);
+            });
+
+          }
+        });
+      }
+    }).catch((error) => {
+      alert(error);
+    });
   }
 
-  updateInfo() {
+  componentWillUnmount = () => {
+    this.isMounted = false;
+    this.robotSnapshot();
+  }
+
+  updateInfo = () => {
     let current = this.state;
-    let robot = this.session.currentRobot();
+    let robot = this.currentRobot;
 
-    current.id = 0;
+    current.id = robot.id;
     current.battery = robot.battery;
+    current.batteryColor = robot.battery > 40 ? "green" : "red";
     current.connection = robot.connection;
+    current.connectionColor = robot.connection > 2 ? "green" : "red";
     current.charging = robot.charging ? "yes" : "no";
-    current.idle = "" //robot.idle_behavior;
-    current.detect = "" //robot.detect_behavior;
+    current.idle = robot.idle_behavior;
+    current.detect = robot.detect_behavior; 
     current.recordings = robot.num_of_videos;
-
-    alert(JSON.stringify(current));
 
     this.setState(current);
   }
@@ -35,14 +90,13 @@ export default class InfoScreen extends Component {
   render() {
     return(
       <View style={styles.container}>
-        <Text style={[{ color: this.state.color}]}>ID: {this.state.id}</Text>
-        <Text style={[{ color: this.state.color}]}>Battery Level: {this.state.battery}</Text>
-        <Text style={[{ color: this.state.color}]}>Wireless Connection Level: {this.state.connection}</Text>
-        <Text style={[{ color: this.state.color}]}>Charging: {this.state.charging}</Text>
-        <Text style={[{ color: this.state.color}]}>Idle Behavior: {this.state.idle}</Text>
-        <Text style={[{ color: this.state.color}]}>Detect Behavior: {this.state.detect}</Text>
-        <Text style={[{ color: this.state.color}]}>Number of Recordings: {this.state.recordings}</Text>
-        <Button title="update" onPress={this.updateInfo} />
+        <Text style={[]}>ID: {this.state.id}</Text>
+        <Text style={[{ color: this.state.batteryColor}]}>Battery Level: {this.state.battery}</Text>
+        <Text style={[{ color: this.state.connectionColor}]}>Wireless Connection Level: {this.state.connection}</Text>
+        <Text style={[]}>Charging: {this.state.charging}</Text>
+        <Text style={[]}>Idle Behavior: {this.state.idle}</Text>
+        <Text style={[]}>Detect Behavior: {this.state.detect}</Text>
+        <Text style={[]}>Number of Recordings: {this.state.recordings}</Text>
       </View>
     );
   }
