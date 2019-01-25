6849ce88794340b0f50508f96f89d16edd052c52
penguinarmy94 <luisote94@gmail.com>
2019-01-24 22:11:12 -0800

    Added placeholders for the UI of the settings functions

---
+++ mobile_code/ROSE/screens/BehaviorEditHomeScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class BehaviorEditHomeScreen extends Component {
+    render() {
+        return(
+            <View style={{flex: 1, justifyContents: "center"}}>
+                <Text>You are now in the BehaviorEditHome Screen</Text>
+            </View>
+        );
+    }
+}
