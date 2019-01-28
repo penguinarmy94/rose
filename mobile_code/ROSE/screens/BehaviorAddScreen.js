eb0ef9b027444f996c3e7318f6d0709520d8a2d6
penguinarmy94 <luisote94@gmail.com>
2019-01-28 14:44:02 -0800

    Added template for adding Behaviors

--- mobile_code/ROSE/screens/BehaviorAddScreen.js
+++ mobile_code/ROSE/screens/BehaviorAddScreen.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 export default class BehaviorAddScreen extends Component {
+    constructor(props) {
+        super(props);
+
+        /*
+        this._actionRef.get().then((snapshot) => {
+            let actionList = [];
+            snapshot.forEach((doc) => {
+                actionList.push(doc.data());
+            });
+            return actionList;
+        }).catch((error) => {
+            alert("No actions available");
+        });
+        */
+    }
+
     render() {
         return(
             <View style={{flex: 1, justifyContents: "center"}}>
