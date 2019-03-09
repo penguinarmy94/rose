8318b5cdeb653aa9aea60c704def535ff2f1fc19
penguinarmy94 <luisote94@gmail.com>
2019-03-08 20:21:57 -0800

    removed brain print function

--- robot_code/build/brain.py
+++ robot_code/build/brain.py
@@ -42,8 +42,6 @@ class Brain():
     
     def begin(self):
         #Run for as long as queues are active
-        self.__lightOn = self.__robot.light
-
         while self.__robot.power is True:
             try:
                 #Read Motor queue for new updates
@@ -180,7 +178,6 @@ class Brain():
             else:
                 return
         if not self.__lightOn == self.__robot.light:
-            print(str(self.__lightOn) + " " + self.__robot.light)
             self.__lightOn = self.__robot.light
             if self.__lightOn is True:
                 message = "turn on"
