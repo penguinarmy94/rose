a97a6eb3fb1d7d1822aae5a654f5d619fcb0f913
Matthias Reimann <matthias.reimann@gmail.com>
2019-03-16 00:18:18 -0800

    .

--- robot_code/build/camera.py
+++ robot_code/build/camera.py
@@ -45,7 +45,7 @@ class Camera():
         if message_packet["type"] == "position":
             message_packet = json.loads(self.__queue.get())
             logger.write(str(datetime.datetime.now()) + " - Brain to Camera: Camera Message Received -- " + message_packet["message"])
-            pwm_servo.ChangeDutyCycle(float(message_packet["message"])
+            pwm_servo.ChangeDutyCycle(float(message_packet["message"]))
             return 1
         elif message_packet["type"] == "off":
             message_packet = json.loads(self.__queue.get())
