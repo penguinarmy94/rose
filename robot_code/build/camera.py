9c329ab6dd5d0e14effdf796bd5a67d630bd5e6b
Matthias Reimann <matthias.reimann@gmail.com>
2019-03-23 16:00:15 -0800

    .

--- robot_code/build/camera.py
+++ robot_code/build/camera.py
@@ -3,15 +3,17 @@ from threading import Thread
 import json, datetime, time, pyttsx3, functools, RPi.GPIO as gpio
 
 class Camera():
+    __pos = None
     __pin = None
     __isOn = False
     __queue = None
     __servo = None
 
-    def __init__(self, queue = None, pin = None):
+    def __init__(self, queue = None, pin = None, pos = 5):
         if queue and pin:
             logger.write(str(datetime.datetime.now()) + " - Camera initialized")
             self.__pin = pin
+            self.__pos = pos
             self.__queue = queue
             
         else:
@@ -21,8 +23,8 @@ class Camera():
         gpio.setwarnings(False)
         gpio.setmode(gpio.BOARD)
         gpio.setup(self.__pin, gpio.OUT)
-        self.__servo = gpio.PWM(self.__pin, 100)
-        self.__servo.start(7)
+        self.__servo = gpio.PWM(self.__pin, 50)
+        self.__servo.start(self.__pos)
 
         while True:
             if not self.__queue.empty():
@@ -46,7 +48,7 @@ class Camera():
             message_packet = json.loads(self.__queue.get())
             logger.write(str(datetime.datetime.now()) + " - Brain to Camera: Camera Message Received -- " + message_packet["message"])
             #self.__servo.start(float(message_packet["message"]))
-            self.__servo.ChangeDutyCycle(float(message_packet["message"]))
+            self.__servo.ChangeDutyCycle(float(self.__pos + message_packet["message"]))
             return 1
         elif message_packet["type"] == "off":
             message_packet = json.loads(self.__queue.get())
