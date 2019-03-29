9d4c38e4ad108f626b0a2ac3261a9b77c1206b6b
penguinarmy94 <luisote94@gmail.com>
2019-03-28 20:40:24 -0700

    Modified UI

--- mobile_code/ROSE/navigators/InfoStack.js
+++ mobile_code/ROSE/navigators/InfoStack.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {createStackNavigator, createAppContainer} from 'react-navigation';
+import {Icon} from 'react-native-elements';
 import InfoScreen from '../screens/InfoScreen';
 
 
@@ -23,6 +24,14 @@ const infoNav = createStackNavigator(
 const InfoContainer =  createAppContainer(infoNav);
 
 export default class InfoStack extends Component {
+  static navigationOptions = ({navigation}) => {
+    return({
+      tabBarIcon: ({tintColor}) => {
+        return(<Icon color={tintColor} type="material-community" name="information-outline" />);
+    }
+    });
+   }
+
   render() {
     return <InfoContainer screenProps={{rootNav: this.props.screenProps.rootNav, data: this.props.screenProps.data}} />
   }
