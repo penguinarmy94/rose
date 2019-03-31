f403059ad7311b8993cd0a13d50a1e9265300a2d
Matthias Reimann <matthias.reimann@gmail.com>
2019-03-31 01:14:29 -0800

    .

--- robot_code/build/camera.py
+++ robot_code/build/camera.py
@@ -70,7 +70,7 @@ class Camera():
             logger.write(str(datetime.datetime.now()) + " - Brain to Camera: Camera Message Received -- " + message_packet["message"] + " -- Moving to " + str(pos))
            
             #self.__servo.ChangeDutyCycle(pos)
-            self.__servo.start(self.__pos)
+            self.__servo.start(pos)
             sleep(1)
             self.__servo.stop()
             return 1
