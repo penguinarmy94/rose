2fd9095545a0f54adeba2e63025e67562dbfe4d4
penguinarmy94 <luisote94@gmail.com>
2019-04-06 18:18:08 -0700

    New microphone class, updated Classifier

--- robot_code/build/microphone.py
+++ robot_code/build/microphone.py
@@ -1,28 +1,77 @@
-#arecord -D dmic_sv -c2 -r 48000 -f S32_LE -t wav -V mono -v recording.wav -d 30
-import json
+from . import logger
+from threading import Thread
+import json, datetime, time, subprocess
+from . import classifier
 
 class Microphone():
-    __micQueue = None
-
-    def __init__(self, queue):
-        self.__micQueue = queue
-
+    __queue = None
+    __config = None
+    __classifier = None
+    
+    def __init__(self,queue = None, config = None):
+        self.__queue = queue
+        self.__config = config
+        self.__classifier = classifier.Classifier(config)
+        
     def run(self):
         while True:
-            self.record()
-            isThreat = self.getClassification()
-            self.writeBrain()
-    
-    def record(self):
-        record = ""
-        print(record)
-    
-    def getClassification(self):
-        classify = ""
-        print(classify)
-    
-    def writeBrain(self):
-        message_packet = { "type": "brain", "message": "threat"}
+            if not self.__queue.empty():
+                result = self.__read_queue()
+                if result == 2:
+                    break
+                else:
+                    continue
+            else:
+                #run the rest of the logic
+                #start microphone, record()
+                #classify it# 
+                file_path = self.__record()
+            
+                isThreat,percentage = self.__classify(file_path)        
+               
+        logger.write(str(datetime.datetime.now()) + " - Microphone: Powered Off")
+           
+    def __read_queue(self):
+        message_packet = json.loads(self.__queue.peek())
 
-        self.__micQueue.put(json.dumps(message_packet))
+        if message_packet["type"] == "off":
+            message_packet = json.loads(self.__queue.get())
+            logger.write(str(datetime.datetime.now()) + " - Brain to Microphone: Off Message Received --" + message_packet["message"])
+            return 2
+        else:
+            return 0
+
+    def __record(self):
+        try:
+            file_path = self.__config["capture_path"] + "/image_" + datetime.datetime.now().strftime("%Y%m%d.%H:%M:%S") + ".wav"
+            subprocess.check_output("arecord -D dmic_sv -c2 -r 44100 -f S32_LE -t "+ self.__config["record_time"] + "wav -V mono -v " + file_path)
+    
+            return file_path
         
+        except Exception as e:
+            print(str(e))
+
+
+    def __classify(self, file_path):
+        #call classify
+        isThreat,percentage = self.__classify(file_path)
+        
+        # if true, send a message to the the brain saying it is a threat 
+        if isThreat:
+            self.__write_queue("Threat Detected")
+        
+        logger.write(str(datetime.datetime.now()) + " - Microphone : Threat Detected with " + percentage + " confidence")
+    
+        
+    def __write_queue(self, message):
+        logger.write(str(datetime.datetime.now()) + " - Microphone to Brain: " + message)
+        try:
+            self.__queue.put(json.dumps({"type": "brain", "message":message}))
+        except Exception as e:
+            error_message = "Microphone.__write_queue() Error: " + str(e)
+            time_stamp = str(datetime.datetime.now())
+        
+            print(time_stamp + ": " + error_message)
+            logger.write(time_stamp+ ": " +error_message)
+
+
