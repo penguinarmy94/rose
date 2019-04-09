d674a6407a3b68c27fbe1a6f8803495f50db6862
Matthias Reimann <matthias.reimann@gmail.com>
2019-04-09 13:13:58 -0800

    .

--- robot_code/app.py
+++ robot_code/app.py
@@ -49,18 +49,18 @@ path.insert(0, config["home_path"])
 
 if args.purge:
     for aPath in [config['log_path'], config['capture_path']]:
-		try:
-			for aFile in os.listdir(aPath):
-				aFilePath = os.path.join(aPath, aFile)
-		
-				if os.path.isfile(aFilePath):
-					if args.verbose:
-						print("Deleting {}...".format(aFilePath))
-					os.unlink(aFilePath)
-	
-		except Exception as e:
-			if args.verbose:
-				print(e)
+        try:
+            for aFile in os.listdir(aPath):
+                aFilePath = os.path.join(aPath, aFile)
+                
+                if os.path.isfile(aFilePath):
+                    if args.verbose:
+                        print("Deleting {}...".format(aFilePath))
+                        os.unlink(aFilePath)
+                        
+        except Exception as e:
+            if args.verbose:
+                print(e)
 
 from build import brain, motor, robot, database
 from build import queues, logger, speaker, notification_manager
