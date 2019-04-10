16a60ba4554b780fe81c664b1307c7704b22cb51
Matthias Reimann <matthias.reimann@gmail.com>
2019-04-09 17:38:24 -0800

    .

--- robot_code/app.py
+++ robot_code/app.py
@@ -182,7 +182,8 @@ def init():
             print("Waiting for robot initialization...")
             time.sleep(0.2)
 
-        option = input('0 - Start in Mobile Mode\n1 - Start in Single Use Mode\n2 - Start in Command Line Mode\n\nChoice: ')
+        #option = input('0 - Start in Mobile Mode\n1 - Start in Single Use Mode\n2 - Start in Command Line Mode\n\nChoice: ')
+        option = 0
         
         if option == "0":
             while True:
