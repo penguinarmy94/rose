d6c362b70f37d47903c5d77577e6554a36dd98cc
penguinarmy94 <luisote94@gmail.com>
2019-04-13 17:13:36 -0700

    Adding Jackhammer data

--- robot_code/classifier_training.py
+++ robot_code/classifier_training.py
@@ -2,4 +2,4 @@ from pyAudioAnalysis import audioTrainTest as aT
 from sys import path
 import json
 
-aT.featureAndTrain(["assets/gun_shot", "assets/car_horn"],1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "randomforest", "randomforestModel", False )
+aT.featureAndTrain(["assets/gun_shot", "assets/car_horn", "assets/jackhammer"],1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "svmModel", False )
