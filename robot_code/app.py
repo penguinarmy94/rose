ac600158cb5c07f4c1a64581cdf9041bd593263f
Matthias Reimann <matthias.reimann@gmail.com>
2019-04-14 23:19:52 -0800

    .

--- robot_code/app.py
+++ robot_code/app.py
@@ -411,7 +411,7 @@ def initialize_threads2(db, rob, off = True):
                     if value == "":
                         if arglist[0]:
                             try:
-                                print("{} : {}".format(config[arglist[0]))
+                                print("{} : {}".format(config[arglist[0]]))
                                 isError = ''
                             except:
                                 error = "Invalid config value. Type 'config' for a lst of all values."
