532c15c85d534025fa980bf92830046e2dd38630
penguinarmy94 <luisote94@gmail.com>
2019-04-19 19:14:48 -0700

    Instead of remove, keep files in capture

--- robot_code/build/uploader.py
+++ robot_code/build/uploader.py
@@ -126,6 +126,8 @@ class Uploader():
             tempFile = join(self.__config["capture_path"], file)
             if isfile(tempFile):
                 print(tempFile)
+                if tempFile.startswith("x_"):
+                    continue
                 if tempFile.endswith(".jpeg") or tempFile.endswith(".jpg") or tempFile.endswith(".wav"):
                     self.__upload(file, tempFile)
 
@@ -153,7 +155,11 @@ class Uploader():
                     self.__robot.videos.append(storage_path)
                     self.__robot.num_of_videos += 1
                     self.__write_queue(message_type="brain", message="upload complete")
-                os.remove(file_path)
+                
+                dir_items = file_path.split("/")
+                dir_items[-1:] = "x_" + dir_items[-1:]
+                new_path = "/".join(dir_items)
+                os.rename(file_path, new_path)
         except Exception as e:
             print(str(e))
     
