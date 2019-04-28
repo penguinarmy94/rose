fe132c93ca781311811f190a643ac8cda4984372
Andrew Javier <diarkia124@yahoo.com>
2019-04-28 15:23:05 -0700

    Simplified obstacle avoidance, motors now run at full speed to compensate for weight of chassis

--- arduino_code/build/main/motor.cpp
+++ arduino_code/build/main/motor.cpp
@@ -1,31 +1,66 @@
 #include "Arduino.h"
-#include "Motor.h"
+#include "motor.h"
 
-Motor::Motor(int lowIn, int highIn, int en)
+#define MOTOR_MIN_SPEED 80
+#define MOTOR_MAX_SPEED 255
+
+Motor::Motor(int in1, int in2, int en)
 {
-  pinMode(lowIn, OUTPUT);
-  pinMode(highIn, OUTPUT);
-  pinMode(en, OUTPUT);
-  kLowIn = lowIn;
-  kHighIn = highIn;
+  kIn1 = in1;
+  kIn2 = in2;
   kEn = en;
+  speed = 0;
+  
+  pinMode(in2, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(en, OUTPUT);
+  
 }
 
-void Motor::forward(int speed)
+void Motor::setForward()
 {
-  digitalWrite(kLowIn, HIGH);
-  digitalWrite(kHighIn, LOW);
-  analogWrite(kEn, speed);
+  digitalWrite(kIn1, LOW);
+  digitalWrite(kIn2, HIGH);
+  this->halt();
 }
 
-void Motor::backward(int speed)
+void Motor::setForward(int speed)
 {
-  digitalWrite(kLowIn, LOW);
-  digitalWrite(kHighIn, HIGH);
-  analogWrite(kEn, speed);
+  digitalWrite(kIn1, LOW);
+  digitalWrite(kIn2, HIGH);
+  this->setSpeed(speed);
+}
+
+void Motor::setReverse()
+{
+  digitalWrite(kIn1, HIGH);
+  digitalWrite(kIn2, LOW);
+  this->halt();
+}
+
+void Motor::setReverse(int speed)
+{
+  digitalWrite(kIn1, HIGH);
+  digitalWrite(kIn2, LOW);
+  this->setSpeed(speed);
 }
 
 void Motor::halt()
 {
-  analogWrite(kEn, 0); //Low and Hi pins are irrelevant
+  this->setSpeed(0);
+}
+
+void Motor::setSpeed(int speed) {
+
+  if (speed < MOTOR_MIN_SPEED) 
+    speed = 0;
+  else if (speed > MOTOR_MAX_SPEED)
+    speed = MOTOR_MAX_SPEED;
+    
+  this->speed = speed;
+  analogWrite(kEn, speed);
+}
+
+int Motor::getSpeed() {
+  return this->speed;
 }
