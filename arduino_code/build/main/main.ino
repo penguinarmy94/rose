912230219a93425cd73d8897bc7082c6bb537e53
Andrew Javier <diarkia124@yahoo.com>
2019-05-02 20:50:30 -0700

    Removed unused experimental code

--- arduino_code/build/main/main.ino
+++ arduino_code/build/main/main.ino
@@ -5,11 +5,11 @@
 
 #define METHOD3
 
-Motor motorRight(6, 5, 7); 
-Motor motorLeft(4, 3, 2); 
+Motor motorRight(4, 3, 2); 
+Motor motorLeft(6, 5, 7); 
 
-Microphone a(0);
-Microphone b(1);
+Microphone a(0); //right mic
+Microphone b(1); //left mic
 LaserSensor lasersensor;
 int aMax;
 int aSample;
@@ -72,9 +72,9 @@ next = 0;
 
 void loop()
 {  
-
 while (!Serial.available())
 {
+
   getSoundSample(a, b, micTime, sampleWindow);
   if(millis() - micTime < sampleWindow)
   {
@@ -90,179 +90,7 @@ while (!Serial.available())
   commandFromPi(piCommand, a, b, motorLeft, motorRight);  
 }
 
-bool checkSensors(unsigned long &time, bool leftsensor, int &prev, int &next, bool &checkprev, bool &checknext)
-{
-  bool result = false; //Default result to false until next is farther than previous
-  if(!checkprev)
-  {
-    prev = (leftsensor) ? laserSensor.getValue(LEFT_SENSOR) : laserSensor.getValue(RIGHT_SENSOR); //Check appropriate sensor value
-
-    //set previous flag to true
-    checknext = false;
-    checkprev = true;
-    time = millis(); //Start timer
-  }
-  
-  else if (millis() - time > 50)
-  {
-    //Code waits 50ms before checking the laser again  
-    next = (leftsensor) ? laserSensor.getValue(LEFT_SENSOR) : laserSensor.getValue(RIGHT_SENSOR);
-
-
-    //Unnecessary but next flag put to true for consistency
-    checkprev = false;
-    checknext = true;
-
-    //Loop ends when next is greater than previous. This happens when robot has moved past the parallel of the wall
-    result = (next > prev);
-  }
-  
-  return result;
-}
-
-void noBlock()
-{
-  if(!turning & !state){
-  Forward(motorLeft, motorRight, 250); //Always go forward if there is nothing in the way
-
-  //Reset the previous and next values for when turning is needed
-  prev = -1;
-  next = 0;
-  }
-}
-
 void readLaser() {  
-  #ifdef METHOD1
-  state = laserSensor.getState(); 
-  if(!turning & !state){
-  noBlock();
-  }
-  
-  else
-  { 
-  if(!turning)
-  {
-    //First instance of something being blocked
-    turnleftsensor = laserSensor.getValue(LEFT_SENSOR) < laserSensor.getValue(RIGHT_SENSOR); //Preference to left, but figure out who turns based on which sensor is less
-
-    //Flags for previous and next being checked are false
-    checkprev = false;
-    checknext = false;
-
-    //Turn flag is set to true
-    turning = true;
-    
-    //Handle turning of obstacle avoidance. Put in this loop so turning is called once.
-    (turnleftsensor) ? Right(left, right, 250) : Left(left, right, 250); //Turn based on which sensor showed farther values
-  }
-  
-  if (checkSensors(time, turnleftsensor, prev, next, checkprev, checknext))
-  {
-    //Reset all flags when turning has gone past the parallel point of the wall
-    turning = false;
-    checkprev = false;
-    checknext = false;
-    Forward(left, right, 250);
-  }
-  }
-  #endif
-  #ifdef METHOD2
-  int distance = laserSensor.getValue(FRONT_SENSOR);
-  if (distance <= SENSOR_STOP_DISTANCE  && !isTurning) {
-    //Serial.println("Start Turning");
-    
-    motorLeft.halt();
-    motorRight.halt();
-
-    if (waitTurn++ > MOTOR_TURN_DELAY) {
-      
-      waitTurn = 0;
-      isTurning = true;
-      isTrackback = false;
-      motorTurnAngle = MOTOR_PREFERRED_TURN_ANGLE;
-      
-      int sensorRead = laserSensor.getValue(LEFT_SENSOR);
-      if (sensorRead <= SENSOR_BLOCK_DISTANCE) {
-        direction = MOTOR_TURN_CLOCKWISE;
-        lastSensorRead = sensorRead;
-      }
-
-      sensorRead = laserSensor.getValue(RIGHT_SENSOR);
-      if (sensorRead <= SENSOR_BLOCK_DISTANCE) {
-        if (direction = MOTOR_TURN_CLOCKWISE) {
-          motorTurnAngle = 180;
-        }
-        else {
-          direction = lastDirection;
-          if (MOTOR_TURN_COUNTERCLOCKWISE == direction) {
-            lastSensorRead = sensorRead;  
-          }
-        }
-      }
-    }   
-  }
-  else if (isTurning) {
-    //Serial.println("Turning");
-
-    if (waitTurn++ > MOTOR_TURN_DURATION) {
-      motorTurnAngle -= MOTOR_ANGLE_PER_ITERATION;
-    
-      if (MOTOR_TURN_CLOCKWISE == direction) {
-          motorLeft.setForward(MOTOR_MIN_SPEED);
-          motorRight.setReverse(MOTOR_MIN_SPEED);
-      }
-      else {
-          motorLeft.setReverse(MOTOR_MIN_SPEED);
-          motorRight.setForward(MOTOR_MIN_SPEED);
-      }
-
-      if (motorTurnAngle <= 0) {
-        isTurning = false;
-      }
-      else {
-        isTurning = !isTrackback;
-      }
-    }
-    else {
-      motorLeft.halt();
-      motorRight.halt();
-
-      if (MOTOR_TURN_CLOCKWISE == direction) {
-        int sensorRead = laserSensor.getValue(LEFT_SENSOR); 
-        sensorRead += laserSensor.getValue(LEFT_SENSOR); 
-        sensorRead += laserSensor.getValue(LEFT_SENSOR);
-        sensorRead = (int) (sensorRead / 3);
-          
-        if (sensorRead/3 <= lastSensorRead + SENSOR_ERROR_BUFFER) {
-          lastSensorRead = sensorRead;     
-        }
-        else {
-          isTrackback = true;
-        }
-      }
-      else {
-        int sensorRead = laserSensor.getValue(RIGHT_SENSOR);
-        sensorRead += laserSensor.getValue(RIGHT_SENSOR);
-        sensorRead += laserSensor.getValue(RIGHT_SENSOR);  
-        sensorRead = (int) (sensorRead / 3);
-        
-        if (sensorRead <= lastSensorRead + SENSOR_ERROR_BUFFER) {
-          lastSensorRead = sensorRead;  
-        }
-        else {
-          isTrackback = true;
-        }
-      }
-
-      if (isTrackback) direction = -direction;
-    }
-  }
-  else {
-    //Serial.println("Driving");   
-    waitTurn = 0;
-  }
-  #endif
-  #ifdef METHOD3
   unsigned long turnStart;
   int frontSensor = laserSensor.getValue(FRONT_SENSOR);
   if(frontSensor <= SENSOR_STOP_DISTANCE){
@@ -295,16 +123,16 @@ void readLaser() {
     }
     else
     {
-      while(millis() - turnStart < 2000)
+      while(millis() - turnStart < 1000)
       {
         (leftSensor < rightSensor) ? Right(motorLeft, motorRight, 255) : Left(motorLeft, motorRight, 255);
       }      
     }
     Halt(motorLeft, motorRight);
+    delay(500);
   }
   else
   {
     Forward(motorLeft, motorRight, 255);
   }
-  #endif
 }
